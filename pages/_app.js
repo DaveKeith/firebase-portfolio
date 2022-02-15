e432624deea2d1f00ae549211ba9f74139bf8beb
DaveKeith <d.keith78@yahoo.com>
2022-02-15 09:53:12 -0500

    made a few changes commented out a few things we're not using yet

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
-import { Fragment } from 'react';
-import { Head } from 'next/document';
+import Head from 'next/head';
 import FBScripts from '../components/scripts';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import Layout from '../components/layout';
 
 const AppComponent = ({ Component, pageProps }) => {
     return (
-        <Fragment>
+        <Layout>
             <Head>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <title>My ReactJS/NextJS/Firebase App</title>
             </Head>
             <FBScripts />
             <Component {...pageProps} />
-        </Fragment>
+        </Layout>
     );
 };
 
