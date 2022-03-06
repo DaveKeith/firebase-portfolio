98b9a71f8318c5b472dc8188188066faeb89ab73
DaveKeith <d.keith78@yahoo.com>
2022-03-06 12:19:47 -0500

    got rid useRouter and went with a vanilla js approach to getting the query

--- pages/projects/[projectId].js
+++ pages/projects/[projectId].js
@@ -1,21 +1,22 @@
-import { Fragment } from "react";
-import { useRouter } from "next/router";
+import { Fragment, useState, useEffect } from "react";
 import Headline from "../../components/header/headline";
-//import { projects } from '../../data/projects.json';
-//import getProjects from "../../utils/getData";
-import path from 'path';
-//import fs from 'fs/promises';
 
-const Project = async () => {
-    const projects = await getProjects();
+const Project = () => {
+    const [myProject, setMyProject] = useState({});
 
-    const { query } = useRouter();
-    const idNum = parseInt(query.projectId);
-    const myProject = projects.find((item) => {
-        return item.projectId === idNum;
-    });
-    const views = myProject.project;
-    const { projectName } = myProject;
+    useEffect(() => {
+        const projectId = window.location.href.split('/').pop();
+        const jsonFileNum = parseInt(projectId) - 1;
+        fetch(
+            `https://davekeith-portfolio-86436-default-rtdb.firebaseio.com/projects/${jsonFileNum}.json`, 
+            { method: "get", mode: "cors" }
+        )
+        .then(res => res.json())
+        .then(res => { setMyProject(res) })
+        .catch(err => console.log(err));
+    }, []);
+
+    const { projectName, views } = myProject;
     const workplace = null;
 
     if(myProject.workplace){
@@ -32,11 +33,13 @@ const Project = async () => {
     </Fragment>
 }
 
-const getProjects = async () => {
-    const jsonData = await fetch('../../data/projects.json', { method: "get", mode: "cors" })
-        .then(res => res.json())
-    const data = JSON.parse(jsonData);
-    return { projects };
-};
+// const getStaticPaths = (context) => {
+//     return {
+//         paths: [
+//             { params: { projectId: context.params.projectId } }
+//         ],
+//         fallback: true
+//     }
+// }
 
 export default Project;
