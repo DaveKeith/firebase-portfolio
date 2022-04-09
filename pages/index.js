9287914580da5c1c66a579832536561c4c66cc73
DaveKeith <d.keith78@yahoo.com>
2022-04-09 10:17:59 -0400

    made more interesting looking shapes on home page links

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const HomePage = () => {
         <section className="content">
             <p className="text-center">Here we break down the site and {"it's"} features.</p>
             <div className="home-grid">
-                <Link href='/about-me' passHref>
+                {/* <Link href='/about-me' passHref>
                     <div className="home-grid__item home-grid__item--left">
                         <h2>About Me</h2>
                         <p>Get to know a little about my background.</p>
@@ -31,6 +31,38 @@ const HomePage = () => {
                     <div className="home-grid__item home-grid__item--right">
                         <h2>Resume</h2>
                         <p>Just in case you {"don't"} have my resume on hand.</p>
+                    </div>
+                </Link> */}
+                <Link href='/about-me' passHref>
+                    <div className="home-grid__item home-grid__item--left">
+                        <div>
+                            <h2>About Me</h2>
+                            <p>Get to know a little about my background.</p>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/about-the-site' passHref>
+                    <div className="home-grid__item home-grid__item--right">
+                        <div>
+                            <h2>About The Site</h2>
+                            <p>Shows what technologies go into making this site.</p>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/projects' passHref>
+                    <div className="home-grid__item home-grid__item--left">
+                        <div>
+                            <h2>Projects</h2>
+                            <p>Gives a list of some of my past projects.</p>
+                        </div>
+                    </div>
+                </Link>
+                <Link href='/resume' passHref>
+                    <div className="home-grid__item home-grid__item--right">
+                        <div>
+                            <h2>Resume</h2>
+                            <p>Just in case you {"don't"} have my resume on hand.</p>
+                        </div>
                     </div>
                 </Link>
             </div>
